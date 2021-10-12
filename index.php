e8f40b580435b287f13320b9bf2037d5fc43aaa9
pennylane <pennylane@pennylane.name>
2021-10-12 20:50:50 +0200

    Refined styles and index stub

--- index.php
+++ index.php
@@ -4,23 +4,50 @@
 
     <head>
         <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
-        <?php print '<link rel="stylesheet" href="styles/main-light.css">' ?>
+        <?php print '<link rel="stylesheet" href="styles/main-dark.css">' ?>
     </head>
 
     <body>
 
         <div id='head'>
             <img class='head_img left' src="assets/sunflower-gnome.png" alt="Sunflower & Garden Gnome">
-            <h1 id='site_title'> Open Neighbor Hub </h1>
+            <h1 id='site_title'> OPEN NEIGHBOR HUB </h1>
             <h2 id='site_subtitle'> Your friendly neighborhood portal </h2>
             <img class='head_img right' src="assets/sunflower-gnome.png" alt="Sunflower & Garden Gnome">
         </div>
         <hr>
 
         <div id='main_menu'>
-            
+            <div class='menu_elem active'>
+                <i class="fas fa-home"></i> Home
+            </div>
+            <div class='menu_elem'>
+                <i class="far fa-lightbulb"></i> Wiki
+            </div>
+            <div class='menu_elem'>
+                <i class="fas fa-glass-cheers"></i> Events
+            </div>            
+            <div class='menu_elem'>
+                <i class="fas fa-book-open"></i> Blog 
+            </div>            
+            <div class='menu_elem prelast'>
+                <i class="fas fa-user-circle"></i> Profile
+            </div>            
+            <div class='menu_elem last'>
+                <i class="fas fa-cogs"></i> Settings
+            </div>            
         </div>
 
+        <div id='main-container'>
+            <h3>Welcome</h3>
+
+            <h4> to our </h4>
+
+            <h5> awesome </h5>
+
+            <h6> neighborhood </h6>
+        </div>
+    
     </body>
 
 </html>
