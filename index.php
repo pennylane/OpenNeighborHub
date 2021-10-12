ef7c4958c203d6237c8a193520381bc73a6a203a
pennylane <pennylane@pennylane.name>
2021-10-12 17:21:18 +0200

    Added index skel and first style

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
+        <?php print '<link rel="stylesheet" href="styles/main-light.css">' ?>
+    </head>
+
+    <body>
+
+        <div id='head'>
+            <img class='head_img left' src="assets/sunflower-gnome.png" alt="Sunflower & Garden Gnome">
+            <h1 id='site_title'> Open Neighbor Hub </h1>
+            <h2 id='site_subtitle'> Your friendly neighborhood portal </h2>
+            <img class='head_img right' src="assets/sunflower-gnome.png" alt="Sunflower & Garden Gnome">
+        </div>
+        <hr>
+
+        <div id='main_menu'>
+            
+        </div>
+
+    </body>
+
+</html>
